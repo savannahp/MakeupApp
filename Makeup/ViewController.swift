17c7e6bb7f8c251acfa2648c6e8bbbfe3a422ca3
Savannah Pyle <bea12022@byui.edu>
2018-05-27 00:00:31 -0600

    here here

--- Makeup/ViewController.swift
+++ Makeup/ViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func showAnswer(_ sender: AnyObject) {
+        answerLabel.text = elementList[currentElementIndex];
+    }
+    
+    @IBAction func gotoNextElement(_ sender: AnyObject) {
+        currentElementIndex += 1;
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0;
+        }
+        updateElement();
+    }
+    
+    let elementList = ["testmakeup", "testmakeup2", "testmakeup3", "testmakeup4"];
+    var currentElementIndex = 0;
+    
+    func updateElement () {
+        answerLabel.text = "?";
+        let elementName = elementList[currentElementIndex];
+        let image = UIImage(named: elementName);
+        imageView.image = image;
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
